cac8cc6fec37d37c176eb652588c42fca87f0133
sian <peter@QA.com>
2024-09-03 14:28:49 +0100

    test sw

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("SianWhale"))
+        System.out.println(sayHelloToSomeone("SianWhale"));
         System.out.println(sayGoodbye());
     }
 
